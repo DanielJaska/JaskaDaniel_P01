f8beb9c250018f5f3a3fd63203f231c22b7be887
Daniel Jaska <dtj190000@utdallas.edu>
2020-08-23 23:33:35 -0500

    P01 Version 01

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _objectToFollow = null;
+
+    Vector3 _objectOffset;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _objectOffset = this.transform.position - _objectToFollow.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = _objectToFollow.position + _objectOffset;
+    }
+}
